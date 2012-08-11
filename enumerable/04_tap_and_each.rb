58b8b475790510bbf6c4521aa2902865fe4ab9b1
okitan <okitakunio@gmail.com>
2012-08-11 16:39:48 +0900

    done 04

--- enumerable/04_tap_and_each.rb
+++ enumerable/04_tap_and_each.rb
@@ -1,11 +1,24 @@
-{}.tap do |result|
-  [:foo, :bar, :hoge, :piyo].each do |type| 
-    records = SomeModel.where(type: type)
-    result[type] = [].tap do |record_array|
-      records.each do |record|
-        r = record.to_hash
-        record_array.push r
-      end
+#!/usr/bin/env ruby
+
+class SomeModel
+  class << self
+    def where(opts = {})
+      key = opts[:type]
+      10.times.map {|n| self.new(key => n) }
     end
   end
+
+  attr_reader :to_hash
+
+  def initialize(hash)
+    @to_hash = hash
+  end
 end
+
+types = [ :foo, :bar, :hoge, :piyo]
+
+result = types.map.with_object({}) do |type, hash|
+  hash[type] = SomeModel.where(:type => type).map(&:to_hash)
+end
+
+p result
