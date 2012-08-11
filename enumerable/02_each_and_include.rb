9742338d80646dc47089e4bbdf64b9a36f38b202
okitan <okitakunio@gmail.com>
2012-08-11 16:16:35 +0900

    done 02
    (but this is not good)

--- enumerable/02_each_and_include.rb
+++ enumerable/02_each_and_include.rb
@@ -1,13 +1,36 @@
+#!/usr/bin/env ruby
 # Somemodel.conditions => Array or nil
 # target => given parameter (must be symbol)
+
+target = $*.first
+$target = target # XXX:
+
+class SomeModel
+  attr_reader :i
+
+  def initialize(i)
+    @i = i
+  end
+
+  def conditions
+    i % 2 == 0 ? %w[ $target ] : nil  # umhhh
+  end
+
+  def inspect
+    "SomeModel:#{i}"
+  end
+
+  class << self
+    def where(opts = {})
+      10.times.map.with_index {|n| SomeModel.new(n) }
+    end
+  end
+end
 
 result = []
 records = SomeModel.where(some_flag: true)
 
-records.each do |r|
-  if !r.conditions
-    records << r
-  elsif r.conditions.include?(target)
-    records << r
-  end
-end
+p records
+
+# not so beautiful
+p result = records.keep_if {|e| !e.conditions || e.conditions.include?(target) }
