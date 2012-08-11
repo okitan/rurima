9d1cb879b910c7e1933131794f5c10398924de9c
okitan <okitakunio@gmail.com>
2012-08-11 16:03:21 +0900

    done 00

--- enumerable/00_generate_hash.rb
+++ enumerable/00_generate_hash.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 # テーマ：配列からハッシュを作る
 
@@ -6,10 +7,18 @@ expected = { 'key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'}
 # (1) keyとvalueが交互に並んだ配列から
 kv = ['key1', 'value1', 'key2', 'value2', 'key3', 'value3']
 
+p actual1 = Hash[*kv]
+p expected == actual1
+
 # (2) keyとvalueがペアになって並んだ配列から
 kvs = [['key1', 'value1'], ['key2', 'value2'], ['key3', 'value3']]
+
+p actual2 = Hash[*kv.flatten]
+p expected == actual2
 
 # (3) keyの配列とvalueの配列から
 keys = ['key1', 'key2', 'key3']
 values =  ['value1', 'value2', 'value3']
 
+p actual3 = Hash[*keys.zip(values).flatten]
+p expected == actual3
