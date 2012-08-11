bdb347846bece2c1400c2d00d2cf548b78c944f9
okitan <okitakunio@gmail.com>
2012-08-11 16:32:02 +0900

    done 05

--- enumerable/05_reverse_string.rb
+++ enumerable/05_reverse_string.rb
@@ -1,3 +1,28 @@
+#!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 # 文字列を逆順にして返す関数を作成してください。
 # ただし Array#reverse, Array#reverse! は使用禁止です。
+
+string = "okitan desu"
+
+# I answered before
+reversed_string = ""
+string.length.times do |i|
+  reversed_string[i] = string[string.length-i-1]
+end
+p reversed_string
+
+# or
+reversed_string = string.each_char.with_object([]) do |c, array|
+  array.unshift(c)
+end.join("")
+
+p reversed_string
+
+# or this
+string.instance_eval do
+  alias each each_char
+end
+string.extend Enumerable
+
+p string.inject([]) {|array, c| array.unshift(c) }.join("")
