cce0ba3ba3517bde902f15835210aaa3b2f895a2
okitan <okitakunio@gmail.com>
2012-08-11 16:21:11 +0900

    done 04

--- enumerable/03_tap_and_map.rb
+++ enumerable/03_tap_and_map.rb
@@ -1,8 +1,13 @@
+#!/usr/bin/env ruby
 # @option is parsed query parameter
 # e.g. { 'foo' => 'bar', 'hoge' => 'piyo'
 
-{}.tap do |hash|
-  @option.map do |k, v|
-    hash[k.to_s.camelize(:lower)] = v
-  end
+require "facets/string/camelcase"
+
+hash = { "foo" => "bar", "hoge" => "piyo", "example_here" => "homuhomu" }
+
+camelized_hash = hash.keys.each.with_object({}) do |key, h|
+  h[key.to_s.camelcase(:upper)] = hash[key]
 end
+
+p camelized_hash
